34ff00658b352bd09b3f81905ab68174c70632af
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-10-16 07:16:58 +0000

    y

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,9 @@ import React from "react";
 function Welcome({name}){
     return (
         <>
-            <p>Hey !</p>
-            <h1>{name}</h1>
-            <h2>Welcome to School</h2>
+            <p>Hey ! <h1>{name}</h1>
+            <h2>Welcome to School</h2></p>
+            
         </>
     )
 }
